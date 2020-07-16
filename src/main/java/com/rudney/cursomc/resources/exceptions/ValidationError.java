8f22790ed3093e198896ba8c0c0b46d4160354c7
Rudney Eduardo <rudneyeduardo.souza@gmail.com>
2020-07-16 14:40:16 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/rudney/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,29 @@
+package com.rudney.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	
+	private static long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void  addError(String fieldname, String message){
+		errors.add(new FieldMessage(fieldname, message));
+		
+	}
+	
+	
+	
+
+}
