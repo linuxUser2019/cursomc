6e17b7231f6d10fd2c975ac17b5c56890ab7eefc
Rudney Eduardo <rudneyeduardo.souza@gmail.com>
2020-07-15 22:52:25 -0300

    Endpoint clientes/{id} pronto

--- src/main/java/com/rudney/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/rudney/cursomc/repositories/ClienteRepository.java
@@ -3,7 +3,7 @@ package com.rudney.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.rudney.cursomc.domain.Categoria;
+
 import com.rudney.cursomc.domain.Cliente;
 
 @Repository
