d9fa7255bd215666fefa048064dd99f506a310e0
Rudney Eduardo <rudneyeduardo.souza@gmail.com>
2020-07-16 01:00:04 -0300

    EndPoint /pedidos/{id} working

---
+++ src/main/java/com/rudney/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.rudney.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rudney.cursomc.domain.Pedido;
+import com.rudney.cursomc.repositories.PedidoRepository;
+import com.rudney.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id){
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
