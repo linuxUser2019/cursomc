646122b718fac4125d0e36c0c2cce5e5689efb54
Rudney Eduardo <rudneyeduardo.souza@gmail.com>
2020-07-16 12:51:50 -0300

    Criando o handler de erro de categoria deletada além do DELETE em categoria

--- src/main/java/com/rudney/cursomc/resources/exceptions/ResourceExceptionhandler.java
+++ src/main/java/com/rudney/cursomc/resources/exceptions/ResourceExceptionhandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.rudney.cursomc.services.exceptions.DataIntegrityException;
 import com.rudney.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -19,6 +20,16 @@ public class ResourceExceptionhandler {
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+		
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 		
 		
 	}
--- src/main/java/com/rudney/cursomc/services/CategoriaService.java
+++ src/main/java/com/rudney/cursomc/services/CategoriaService.java
@@ -3,13 +3,13 @@ package com.rudney.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.rudney.cursomc.domain.Categoria;
 import com.rudney.cursomc.repositories.CategoriaRepository;
+import com.rudney.cursomc.services.exceptions.DataIntegrityException;
 import com.rudney.cursomc.services.exceptions.ObjectNotFoundException;
-
-
 
 @Service
 public class CategoriaService {
@@ -17,25 +17,30 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id){
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
-	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
-	
-	
-}
 
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
+	}
+
+}
