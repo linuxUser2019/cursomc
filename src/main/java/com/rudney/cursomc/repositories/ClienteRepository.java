00a3cfa95563c7ec9b7c6965d7291e42b57c8596
Rudney Eduardo <rudneyeduardo.souza@gmail.com>
2020-07-16 18:07:55 -0300

    Validação email

--- src/main/java/com/rudney/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/rudney/cursomc/repositories/ClienteRepository.java
@@ -1,14 +1,16 @@
 package com.rudney.cursomc.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 
 import com.rudney.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	
-	
+	@Transactional
+	Cliente findByEmail(String email);
 
 }
