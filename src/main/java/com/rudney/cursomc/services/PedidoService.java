6e9626a174b18fb3cb7c8fd733b9380d43d2f0b1
Rudney Eduardo <rudneyeduardo.souza@gmail.com>
2020-07-17 00:15:54 -0300

    Config Spring Security

--- src/main/java/com/rudney/cursomc/services/PedidoService.java
+++ src/main/java/com/rudney/cursomc/services/PedidoService.java
@@ -15,43 +15,39 @@ import com.rudney.cursomc.repositories.PagamentoRepository;
 import com.rudney.cursomc.repositories.PedidoRepository;
 import com.rudney.cursomc.services.exceptions.ObjectNotFoundException;
 
-
-
 @Service
 public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
-	
 
-	public Pedido buscar(Integer id){
+	public Pedido buscar(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
-	
+
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
-			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());	
-	}
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
@@ -59,9 +55,9 @@ public class PedidoService {
 			ip.setPreco(produtoService.buscar(ip.getProduto().getId()).getPreco());
 			ip.setPedido(obj);
 		}
-		
+
 		itemPedidoRepository.saveAll(obj.getItens());
 		return obj;
-		
-}
+
+	}
 }
